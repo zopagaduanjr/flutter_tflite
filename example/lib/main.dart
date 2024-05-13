438a23992ac6a81eb37b23a33d8682461b56ab92
Zaldy Jr Pagaduan <zaldypagaduanjr@gmail.com>
2024-05-13 16:42:49 +0800

    updated example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,15 +1,15 @@
-import 'dart:async';
 import 'dart:io';
 import 'dart:math';
-import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:tflite/tflite.dart';
 import 'package:image/image.dart' as img;
 
-import 'package:tflite/tflite.dart';
-import 'package:image_picker/image_picker.dart';
-
-void main() => runApp(new App());
+void main() {
+  runApp(const App());
+}
 
 const String mobile = "MobileNet";
 const String ssd = "SSD MobileNet";
@@ -18,39 +18,39 @@ const String deeplab = "DeepLab";
 const String posenet = "PoseNet";
 
 class App extends StatelessWidget {
+  const App({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: MyApp(),
-    );
+    return const MaterialApp(home: MyApp());
   }
 }
 
 class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
   @override
-  _MyAppState createState() => new _MyAppState();
+  State<MyApp> createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
-  File _image;
-  List _recognitions;
+  File? _image;
+  List? _recognitions;
   String _model = mobile;
-  double _imageHeight;
-  double _imageWidth;
+  double? _imageHeight;
+  double? _imageWidth;
   bool _busy = false;
 
   Future predictImagePicker() async {
-    var image = await ImagePicker.pickImage(source: ImageSource.gallery);
+    var image = await ImagePicker().pickImage(source: ImageSource.gallery);
     if (image == null) return;
     setState(() {
       _busy = true;
     });
-    predictImage(image);
+    predictImage(File(image.path));
   }
 
   Future predictImage(File image) async {
-    if (image == null) return;
-
     switch (_model) {
       case yolo:
         await yolov2Tiny(image);
@@ -69,8 +69,8 @@ class _MyAppState extends State<MyApp> {
       // await recognizeImageBinary(image);
     }
 
-    new FileImage(image)
-        .resolve(new ImageConfiguration())
+    FileImage(image)
+        .resolve(const ImageConfiguration())
         .addListener(ImageStreamListener((ImageInfo info, bool _) {
       setState(() {
         _imageHeight = info.image.height.toDouble();
@@ -100,7 +100,7 @@ class _MyAppState extends State<MyApp> {
   Future loadModel() async {
     Tflite.close();
     try {
-      String res;
+      String? res;
       switch (_model) {
         case yolo:
           res = await Tflite.loadModel(
@@ -142,39 +142,40 @@ class _MyAppState extends State<MyApp> {
     }
   }
 
-  Uint8List imageToByteListFloat32(
-      img.Image image, int inputSize, double mean, double std) {
-    var convertedBytes = Float32List(1 * inputSize * inputSize * 3);
-    var buffer = Float32List.view(convertedBytes.buffer);
-    int pixelIndex = 0;
-    for (var i = 0; i < inputSize; i++) {
-      for (var j = 0; j < inputSize; j++) {
-        var pixel = image.getPixel(j, i);
-        buffer[pixelIndex++] = (img.getRed(pixel) - mean) / std;
-        buffer[pixelIndex++] = (img.getGreen(pixel) - mean) / std;
-        buffer[pixelIndex++] = (img.getBlue(pixel) - mean) / std;
-      }
-    }
-    return convertedBytes.buffer.asUint8List();
-  }
-
-  Uint8List imageToByteListUint8(img.Image image, int inputSize) {
-    var convertedBytes = Uint8List(1 * inputSize * inputSize * 3);
-    var buffer = Uint8List.view(convertedBytes.buffer);
-    int pixelIndex = 0;
-    for (var i = 0; i < inputSize; i++) {
-      for (var j = 0; j < inputSize; j++) {
-        var pixel = image.getPixel(j, i);
-        buffer[pixelIndex++] = img.getRed(pixel);
-        buffer[pixelIndex++] = img.getGreen(pixel);
-        buffer[pixelIndex++] = img.getBlue(pixel);
-      }
-    }
-    return convertedBytes.buffer.asUint8List();
-  }
+  //TODO: update img.getRed, getGreen, getBlue
+  // Uint8List imageToByteListFloat32(
+  //     img.Image image, int inputSize, double mean, double std) {
+  //   var convertedBytes = Float32List(1 * inputSize * inputSize * 3);
+  //   var buffer = Float32List.view(convertedBytes.buffer);
+  //   int pixelIndex = 0;
+  //   for (var i = 0; i < inputSize; i++) {
+  //     for (var j = 0; j < inputSize; j++) {
+  //       var pixel = image.getPixel(j, i);
+  //       buffer[pixelIndex++] = (img.getRed(pixel) - mean) / std;
+  //       buffer[pixelIndex++] = (img.getGreen(pixel) - mean) / std;
+  //       buffer[pixelIndex++] = (img.getBlue(pixel) - mean) / std;
+  //     }
+  //   }
+  //   return convertedBytes.buffer.asUint8List();
+  // }
+  //
+  // Uint8List imageToByteListUint8(img.Image image, int inputSize) {
+  //   var convertedBytes = Uint8List(1 * inputSize * inputSize * 3);
+  //   var buffer = Uint8List.view(convertedBytes.buffer);
+  //   int pixelIndex = 0;
+  //   for (var i = 0; i < inputSize; i++) {
+  //     for (var j = 0; j < inputSize; j++) {
+  //       var pixel = image.getPixel(j, i);
+  //       buffer[pixelIndex++] = img.getRed(pixel);
+  //       buffer[pixelIndex++] = img.getGreen(pixel);
+  //       buffer[pixelIndex++] = img.getBlue(pixel);
+  //     }
+  //   }
+  //   return convertedBytes.buffer.asUint8List();
+  // }
 
   Future recognizeImage(File image) async {
-    int startTime = new DateTime.now().millisecondsSinceEpoch;
+    int startTime = DateTime.now().millisecondsSinceEpoch;
     var recognitions = await Tflite.runModelOnImage(
       path: image.path,
       numResults: 6,
@@ -185,29 +186,29 @@ class _MyAppState extends State<MyApp> {
     setState(() {
       _recognitions = recognitions;
     });
-    int endTime = new DateTime.now().millisecondsSinceEpoch;
+    int endTime = DateTime.now().millisecondsSinceEpoch;
     print("Inference took ${endTime - startTime}ms");
   }
 
-  Future recognizeImageBinary(File image) async {
-    int startTime = new DateTime.now().millisecondsSinceEpoch;
-    var imageBytes = (await rootBundle.load(image.path)).buffer;
-    img.Image oriImage = img.decodeJpg(imageBytes.asUint8List());
-    img.Image resizedImage = img.copyResize(oriImage, height: 224, width: 224);
-    var recognitions = await Tflite.runModelOnBinary(
-      binary: imageToByteListFloat32(resizedImage, 224, 127.5, 127.5),
-      numResults: 6,
-      threshold: 0.05,
-    );
-    setState(() {
-      _recognitions = recognitions;
-    });
-    int endTime = new DateTime.now().millisecondsSinceEpoch;
-    print("Inference took ${endTime - startTime}ms");
-  }
+  // Future recognizeImageBinary(File image) async {
+  //   int startTime = DateTime.now().millisecondsSinceEpoch;
+  //   var imageBytes = (await rootBundle.load(image.path)).buffer;
+  //   img.Image oriImage = img.decodeJpg(imageBytes.asUint8List());
+  //   img.Image resizedImage = img.copyResize(oriImage, height: 224, width: 224);
+  //   var recognitions = await Tflite.runModelOnBinary(
+  //     binary: imageToByteListFloat32(resizedImage, 224, 127.5, 127.5),
+  //     numResults: 6,
+  //     threshold: 0.05,
+  //   );
+  //   setState(() {
+  //     _recognitions = recognitions;
+  //   });
+  //   int endTime = new DateTime.now().millisecondsSinceEpoch;
+  //   print("Inference took ${endTime - startTime}ms");
+  // }
 
   Future yolov2Tiny(File image) async {
-    int startTime = new DateTime.now().millisecondsSinceEpoch;
+    int startTime = DateTime.now().millisecondsSinceEpoch;
     var recognitions = await Tflite.detectObjectOnImage(
       path: image.path,
       model: "YOLO",
@@ -228,12 +229,12 @@ class _MyAppState extends State<MyApp> {
     setState(() {
       _recognitions = recognitions;
     });
-    int endTime = new DateTime.now().millisecondsSinceEpoch;
+    int endTime = DateTime.now().millisecondsSinceEpoch;
     print("Inference took ${endTime - startTime}ms");
   }
 
   Future ssdMobileNet(File image) async {
-    int startTime = new DateTime.now().millisecondsSinceEpoch;
+    int startTime = DateTime.now().millisecondsSinceEpoch;
     var recognitions = await Tflite.detectObjectOnImage(
       path: image.path,
       numResultsPerClass: 1,
@@ -248,12 +249,12 @@ class _MyAppState extends State<MyApp> {
     setState(() {
       _recognitions = recognitions;
     });
-    int endTime = new DateTime.now().millisecondsSinceEpoch;
+    int endTime = DateTime.now().millisecondsSinceEpoch;
     print("Inference took ${endTime - startTime}ms");
   }
 
   Future segmentMobileNet(File image) async {
-    int startTime = new DateTime.now().millisecondsSinceEpoch;
+    int startTime = DateTime.now().millisecondsSinceEpoch;
     var recognitions = await Tflite.runSegmentationOnImage(
       path: image.path,
       imageMean: 127.5,
@@ -263,12 +264,12 @@ class _MyAppState extends State<MyApp> {
     setState(() {
       _recognitions = recognitions;
     });
-    int endTime = new DateTime.now().millisecondsSinceEpoch;
+    int endTime = DateTime.now().millisecondsSinceEpoch;
     print("Inference took ${endTime - startTime}");
   }
 
   Future poseNet(File image) async {
-    int startTime = new DateTime.now().millisecondsSinceEpoch;
+    int startTime = DateTime.now().millisecondsSinceEpoch;
     var recognitions = await Tflite.runPoseNetOnImage(
       path: image.path,
       numResults: 2,
@@ -279,7 +280,7 @@ class _MyAppState extends State<MyApp> {
     setState(() {
       _recognitions = recognitions;
     });
-    int endTime = new DateTime.now().millisecondsSinceEpoch;
+    int endTime = DateTime.now().millisecondsSinceEpoch;
     print("Inference took ${endTime - startTime}ms");
   }
 
@@ -291,12 +292,13 @@ class _MyAppState extends State<MyApp> {
     });
     await loadModel();
 
-    if (_image != null)
-      predictImage(_image);
-    else
+    if (_image != null) {
+      predictImage(_image!);
+    } else {
       setState(() {
         _busy = false;
       });
+    }
   }
 
   List<Widget> renderBoxes(Size screen) {
@@ -304,9 +306,9 @@ class _MyAppState extends State<MyApp> {
     if (_imageHeight == null || _imageWidth == null) return [];
 
     double factorX = screen.width;
-    double factorY = _imageHeight / _imageWidth * screen.width;
-    Color blue = Color.fromRGBO(37, 213, 253, 1.0);
-    return _recognitions.map((re) {
+    double factorY = _imageHeight! / _imageWidth! * screen.width;
+    Color blue = const Color.fromRGBO(37, 213, 253, 1.0);
+    return _recognitions!.map((re) {
       return Positioned(
         left: re["rect"]["x"] * factorX,
         top: re["rect"]["y"] * factorY,
@@ -314,7 +316,7 @@ class _MyAppState extends State<MyApp> {
         height: re["rect"]["h"] * factorY,
         child: Container(
           decoration: BoxDecoration(
-            borderRadius: BorderRadius.all(Radius.circular(8.0)),
+            borderRadius: const BorderRadius.all(Radius.circular(8.0)),
             border: Border.all(
               color: blue,
               width: 2,
@@ -338,10 +340,10 @@ class _MyAppState extends State<MyApp> {
     if (_imageHeight == null || _imageWidth == null) return [];
 
     double factorX = screen.width;
-    double factorY = _imageHeight / _imageWidth * screen.width;
+    double factorY = _imageHeight! / _imageWidth! * screen.width;
 
     var lists = <Widget>[];
-    _recognitions.forEach((re) {
+    _recognitions!.forEach((re) {
       var color = Color((Random().nextDouble() * 0xFFFFFF).toInt() << 0)
           .withOpacity(1.0);
       var list = re["keypoints"].values.map<Widget>((k) {
@@ -382,16 +384,17 @@ class _MyAppState extends State<MyApp> {
                 decoration: BoxDecoration(
                     image: DecorationImage(
                         alignment: Alignment.topCenter,
-                        image: MemoryImage(_recognitions),
+                        image: MemoryImage(_recognitions! as Uint8List),
                         fit: BoxFit.fill)),
-                child: Opacity(opacity: 0.3, child: Image.file(_image))),
+                child: Opacity(opacity: 0.3, child: Image.file(_image!))),
       ));
     } else {
       stackChildren.add(Positioned(
         top: 0.0,
         left: 0.0,
         width: size.width,
-        child: _image == null ? Text('No image selected.') : Image.file(_image),
+        child:
+            _image == null ? Text('No image selected.') : Image.file(_image!),
       ));
     }
 
@@ -399,7 +402,7 @@ class _MyAppState extends State<MyApp> {
       stackChildren.add(Center(
         child: Column(
           children: _recognitions != null
-              ? _recognitions.map((res) {
+              ? _recognitions!.map((res) {
                   return Text(
                     "${res["index"]} - ${res["label"]}: ${res["confidence"].toStringAsFixed(3)}",
                     style: TextStyle(
